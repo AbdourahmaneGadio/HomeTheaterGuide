9d188e3dcf73ade165d98b32b5725848a7b1ba73
Abdourahmane Gadio <91066652+AbdourahmaneGadio@users.noreply.github.com>
2022-04-28 07:27:36 +0200

    Test affiche enceinte Jamo S803

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-//Affiche le prix minimum choisi
+// Affiche le prix minimum choisi
 
 function afficherPrixMin(){
     var selectionPrixMin = document.getElementById("selectionPrixMin");
@@ -12,7 +12,7 @@ function afficherPrixMin(){
 
 
 
-//Affiche le prix maximum choisi
+// Affiche le prix maximum choisi
 
 function afficherPrixMax(){
     var selectionPrixMax = document.getElementById("selectionPrixMax");
@@ -22,5 +22,28 @@ function afficherPrixMax(){
     function(){
         valeurPrixMax.innerHTML = this.value;
     }
+}
+
+
+// Affiche les enceintes selon les critères
+function choixEnceintes(){
+    var tableEnceintes = document.getElementById("tableEnceintes");
+
+    fetch("json/enceintes.json")
+        .then(response => response.json())
+        .then(data => {
+            var imageEnceinte = data['Jamo_S803']["0"]["Image"];
+            var prixEnceinte = data['Jamo_S803']["0"]["Prix"];
+            var couleurEnceinte = data['Jamo_S803']["0"]["Couleur"];
+            var typeEnceinte = data['Jamo_S803']["0"]["Type"];
+
+            tableEnceintes.innerHTML = 
+            "<tr>"
+            + "<td><img src=" + imageEnceinte + " width=400></td>"
+            + "<td>" + prixEnceinte + "</td>"
+            + "<td>"+ couleurEnceinte +"</td>"
+            + "<td>" + typeEnceinte + "</td>"
+            +"</tr>";
+        })
 }
 
